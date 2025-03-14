14bd76eaf5d1ed06f2114f429be470ada09de7be
redxcyber <mrdark9628@gmail.com>
2025-03-14 13:03:24 +0530

    add file to Inner class

---
+++ src/$13_Inner_Classes/_01_InnerClass.java
@@ -0,0 +1,32 @@
+package $13_Inner_Classes;
+
+class Outer{
+    String outerName = "pranav singh";
+    class Inner{
+        String innerName = "atul singh";
+        void display(){
+            System.out.println("\n---- Showing by Inner class ----");
+            System.out.println("Outer name : "+outerName);
+            System.out.println("inner name : "+innerName);
+        }
+    }
+    void display(){
+        System.out.println("\n---- Showing by Outer class ----");
+        System.out.println("Outer name : "+outerName);
+
+        Inner i = new Inner();
+        System.out.println("Inner name : "+i.innerName);
+    }
+}
+public class _01_InnerClass {
+    public static void main(String[] args) {
+
+        //object of Outer class
+        Outer o = new Outer();
+        o.display();
+
+        //object of Inner class
+        Outer.Inner i = new Outer().new Inner();
+        i.display();
+    }
+}
