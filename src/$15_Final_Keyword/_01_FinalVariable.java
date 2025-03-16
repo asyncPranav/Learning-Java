d1dba1b968cf77a0e3d5d437afd118972912d95b
redxcyber <mrdark9628@gmail.com>
2025-03-16 15:54:34 +0530

    add file to final keyword

--- src/$15_Final_Keyword/_01_FinalVariable.java
+++ src/$15_Final_Keyword/_01_FinalVariable.java
@@ -4,12 +4,15 @@ class Test{
     final int MIN = 1;
     final static int NORMAL;
     final int MAX;
-
+    final int X;
     static {
         NORMAL = 5;
     }
     Test(){
         MAX = 10;
+    }
+    {
+        X = 20;
     }
 }
 public class _01_FinalVariable {
@@ -19,5 +22,6 @@ public class _01_FinalVariable {
         System.out.println("NORMAL : "+t.NORMAL);
         System.out.println("NORMAL : "+Test.NORMAL);
         System.out.println("MIN : "+t.MIN);
+        System.out.println("X : "+t.X);
     }
 }
